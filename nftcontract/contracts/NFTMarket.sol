11a3bba1bb8acbc918f2ff61a993effa0e5c082a
Sachin Thapa <hesoyamh2km@gmail.com>
2025-01-15 17:03:52 -0500

    added imports and started smart contract

--- nftcontract/contracts/NFTMarket.sol
+++ nftcontract/contracts/NFTMarket.sol
@@ -6,3 +6,31 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "hardhat/console.sol";
 
+contract NFTMarket is ERC721URIStorage{
+    uint256 private _tokenIds;
+    uint256 private _itemsSold;
+
+    address payable owner;
+
+    mapping(uint256 => MarketItem) private idMarketItem;
+
+    struct MarketItem {
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+    }
+
+    event idMarketItemCreated(
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold
+    );
+
+    constructor() ERC721("NFT Token", "MERONFT"){
+
+    }
+}
